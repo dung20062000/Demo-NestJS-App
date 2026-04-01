0e07b834a37cf71fe9141eebad83eaefbbaaaee8
BT_Dung <dungbuivm@gmail.com>
2026-04-01 08:44:38 +0700

    tao base schema (bang data base)

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-@Schema()
+@Schema({ timestamps: true })
 export class User extends Document {
     @Prop({ required: true })
     name: string;
@@ -11,6 +11,30 @@ export class User extends Document {
 
     @Prop({ required: true })
     password: string;
+
+    @Prop()
+    phone: string;
+
+    @Prop()
+    address: string;
+
+    @Prop()
+    image: string;
+
+    @Prop()
+    role: string;
+
+    @Prop()
+    accountType: string;
+
+    @Prop()
+    isActive: string;
+
+    @Prop()
+    codeId: string;
+
+    @Prop()
+    codeExpired: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
